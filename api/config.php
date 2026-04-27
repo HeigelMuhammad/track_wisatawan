dec85ad7d5714bf19ba869e9951e4a3cfa145618
Heigel.muhammad <147461702+HeigelMuhammad@users.noreply.github.com>
2026-04-28 00:23:45 +0700

    Add SSL CA path configuration for PDO connection

--- api/config.php
+++ api/config.php
@@ -5,6 +5,11 @@ $dbname   = getenv('TIDB_DATABASE');
 $username = getenv('TIDB_USER');
 $password = getenv('TIDB_PASSWORD');
 
+$ca_path = '/etc/ssl/certs/ca-certificates.crt'; 
+if (!file_exists($ca_path)) {
+    $ca_path = '/etc/pki/tls/certs/ca-bundle.crt'; 
+}
+
 try {
     $pdo = new PDO(
         "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", 
@@ -12,6 +17,7 @@ try {
         $password,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_CA => $ca_path,
             PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
         ]
     );
